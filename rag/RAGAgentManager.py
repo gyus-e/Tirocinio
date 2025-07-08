ec36523c8bd29763440e1b19c5b17ed54896377f
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-07-08 19:59:15 +0200

    added context handling to RAGAgentManager

--- rag/RAGAgentManager.py
+++ rag/RAGAgentManager.py
@@ -1,4 +1,5 @@
 from llama_index.core import Settings
+from llama_index.core.workflow import Context
 from llama_index.core.agent.workflow import AgentWorkflow
 from .QueryEngineManager import QueryEngineManager
 from .rag_prompt import build_rag_prompt
@@ -7,14 +8,35 @@ from .ragTools import search_documents
 
 class RAGAgentManager:
     _rag_agent: AgentWorkflow
-    _initialized: bool = False
+    _context: Context
+    __initialized: bool = False
+    __has_context: bool = False
 
     @classmethod
     async def get_rag_agent(cls) -> AgentWorkflow:
-        if not cls._initialized:
+        if not cls.__initialized:
             await cls._init_rag_agent()
 
         return cls._rag_agent
+
+    @classmethod
+    async def get_context(cls) -> Context:
+        if not cls.__initialized:
+            await cls._init_rag_agent()
+
+        if not cls.__has_context:
+            cls._context = Context(cls._rag_agent)
+            cls.__has_context = True
+
+        return cls._context
+
+    @classmethod
+    async def reset_context(cls):
+        if not cls.__initialized or not cls.__has_context:
+            return
+
+        cls._context = Context(cls._rag_agent)
+        return cls._context
 
     @classmethod
     async def _init_rag_agent(cls):
@@ -28,4 +50,4 @@ class RAGAgentManager:
             system_prompt=build_rag_prompt(),
         )
 
-        cls._initialized = True
+        cls.__initialized = True

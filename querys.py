c24dcc242ac13a8d5ef99661f4280d532b306063
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-08-19 14:29:29 +0200

    changed querys

--- querys.py
+++ querys.py
@@ -1,4 +1,12 @@
 querys = [
-    "which Patient experienced issues with blood glucose meter, what was the problem ?",
-    "what is the age of the patient who had a heart pump failure during surgery?",
+    "Quali incunaboli sono stati pubblicati a Venezia e in quali anni?",
+    "Descrivi le caratteristiche fisiche e lo stato di conservazione dell'incunabolo di Caracciolo Roberto (4B1)",
+    # "Confronta le legature e le decorazioni degli incunaboli 4B3 e 4C2",
+    # "Quali autori classici vengono citati più frequentemente nelle opere della collezione e in quale contesto?",
+    # "Quali stampatori veneziani hanno collaborato con più autori presenti nella collezione e quali caratteristiche hanno in comune le loro pubblicazioni?",
+    # "Quali filosofi vengono citati nel Liber tertius dell'opera di Achillini Alessandro e quante volte viene menzionato Averroè nell'intero testo?",
+    # "Come si riflette l'influenza di Platone nelle opere della collezione Pontaniana?",
+    # "Quali marche tipografiche sono presenti negli incunaboli della collezione e cosa rappresentano?",
+    # "Chi è Tommaso di Alessandria e qual è il suo ruolo nelle opere della collezione?",
+    # "Riassumi i principali temi religiosi trattati nella collezione, citando specifici esempi dai testi",
 ]

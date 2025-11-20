0a3032d070314e79d3b9110c09c05b35d14b0922
blackfellows <6dfdac64-b799-4068-9972-4a1ea68da044@blackfellows.anonaddy.me>
2025-11-20 18:20:29 +0100

    removed unused line and imports

--- documents.py
+++ documents.py
@@ -1,9 +1,5 @@
 from llama_index.core import SimpleDirectoryReader
-import sys
-import io
 from config import documents_dir
-
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
 
 documents = SimpleDirectoryReader(documents_dir).load_data()
 doc_text = [doc.text.strip() for doc in documents]
